a0b1217c842070500d72b384e4294f8f9eada191
Santeri Hakoniemi <santeri.hakoniemi@gmail.com>
2020-06-16 21:46:14 +0300

    16.6.2020

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Post(props) {
     return (
         <div className="post" style={props.index % 2 !== 0 ? { backgroundColor: '#ccc' } : {}} >
-            <h1 className="post-title">{props.data.title}</h1>
+            <p className="post-title">{props.data.title}</p>
             <h2 className="post-author">Posted by {props.data.author}</h2>
             <p className="post-message">{props.data.message}</p>
             <p className="post-date">{props.data.date}</p>
